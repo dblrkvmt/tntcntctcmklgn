8f7f745907d7ac2430ef1b195f11360e78e490a3
dblrkvmt <dblrkvmt@gmail.com>
2023-02-05 09:16:29 +0700

    mp3 -> mp4

---
+++ mp32mp4.bat
@@ -0,0 +1,2 @@
+"C:\Users\Admin\Downloads\ffmpeg-2023-02-02-git-7d49fef8b4-essentials_build\bin\ffmpeg.exe" -f lavfi -i color=c=black:s=1280x720:r=5 -i "1122.mp3" -crf 0 -c:a copy -shortest "1122.mp4"
+
